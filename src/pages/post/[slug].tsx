d3b20724831230642bee7898a7c8e8d943112973
Felipe <felipedemelopontes@gmail.com>
2021-04-12 11:29:48 +0200

    feat: post and styles

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Header from '../../components/Header';
+import {FiCalendar, FiUser, FiClock} from 'react-icons/fi'
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -26,9 +28,48 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({post}: PostProps) {
+  return (
+    <>
+      <Header />
+      <img src="/teste.png" alt="image" className={styles.banner}/>
+      <main className={commonStyles.container}>
+        <div className={styles.post}>
+          <div className={styles.postTop}>
+            <h1>Algum titulo de exemplo</h1>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 Mar 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Felipe Pontes
+                </li>
+                <li>
+                  <FiClock />
+                  4 min
+                </li>
+              </ul>
+          </div>   
+
+          <article>
+            <h2>Titulo seção</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Architecto iste error officia, exercitationem expedita atque deserunt, 
+              nihil consequatur natus ab corporis in perspiciatis quos velit maxime ut. 
+              Omnis dolorem dolores accusantium, ipsum soluta minus velit rem sunt possimus 
+              laborum corrupti expedita, dignissimos commodi quos alias nostrum iure vitae illum praesentium 
+              debitis quam doloremque quisquam quis corporis. Itaque quaerat rerum nulla quis omnis reprehenderit, 
+              similique consequatur culpa illo! Nam, quis, enim alias porro velit sapiente odit magni laboriosam unde neque quibusdam?
+            </p>
+          </article>
+        </div>
+      </main>
+    </>
+  )
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient();
